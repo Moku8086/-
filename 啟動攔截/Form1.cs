fa22ac15abda67231fa34ff696ce997f86275555
Kusyun <kusyunexcl@gmail.com>
2020-04-22 13:49:08 +0800

    feat：新增要求管理權限

--- 啟動攔截/Form1.cs
+++ 啟動攔截/Form1.cs
@@ -16,8 +16,15 @@ namespace 啟動攔截 {
         }
 
         public Form1(string fileName) {
-            InitializeComponent();
-            MessageBox.Show(fileName);
+            //InitializeComponent();
+            
+            if (MessageBox.Show("是否要執行\r\n" + fileName, "問題", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+                RegistryKey key = Registry.ClassesRoot.OpenSubKey(".exe");
+                Registry.ClassesRoot.OpenSubKey((string)key.GetValue(null) + "\\shell\\open\\command", true).SetValue(null, "\"%1\" %*");
+                System.Diagnostics.Process.Start(fileName);
+                Registry.ClassesRoot.OpenSubKey((string)key.GetValue(null) + "\\shell\\open\\command", true).SetValue(null, "\"" + Application.ExecutablePath + "\"" + " \"%1\"");
+            }
+            this.Close();
         }
 
         private void button1_Click(object sender, EventArgs e) {
@@ -26,6 +33,7 @@ namespace 啟動攔截 {
         }
 
         private void button2_Click(object sender, EventArgs e) {
+            //System.Diagnostics.Process.Start("C:\\Program Files (x86)\\BEL\\Realterm\\realterm.exe");
             RegistryKey key = Registry.ClassesRoot.OpenSubKey(".exe");
             Registry.ClassesRoot.OpenSubKey((string)key.GetValue(null) + "\\shell\\open\\command", true).SetValue(null, "\"%1\" %*");
         }
