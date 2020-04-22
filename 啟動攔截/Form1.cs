6846892885e41f9c03d51141418f163e8b283a13
Kusyun <kusyunexcl@gmail.com>
2020-04-22 13:23:23 +0800

    加入專案檔案。

---
+++ 啟動攔截/Form1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 啟動攔截 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        public Form1(string fileName) {
+            InitializeComponent();
+            MessageBox.Show(fileName);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            RegistryKey key = Registry.ClassesRoot.OpenSubKey(".exe");
+            Registry.ClassesRoot.OpenSubKey((string)key.GetValue(null) + "\\shell\\open\\command", true).SetValue(null, "\"" + Application.ExecutablePath + "\"" + " \"%1\"");
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            RegistryKey key = Registry.ClassesRoot.OpenSubKey(".exe");
+            Registry.ClassesRoot.OpenSubKey((string)key.GetValue(null) + "\\shell\\open\\command", true).SetValue(null, "\"%1\" %*");
+        }
+    }
+}
---
+++ 啟動攔截/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 啟動攔截 {
+    static class Program {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args) {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            if (args.Length != 0) {
+                Form1 f = new Form1(args[0]);
+
+                Application.Run(f);
+            } else {
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
